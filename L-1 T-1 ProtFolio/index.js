3e9078473dcf38dbd61b829ce2a46a2e1ae8fda8
Bilal <bilalshaikh2937@gmail.com>
2024-02-17 12:28:18 +0530

    Assessments
    Completed Level 1 tasks

---
+++ L-1 T-1 ProtFolio/index.js
@@ -0,0 +1,41 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+let sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+
+// Google Sheet Attachment
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzxSdh5LsizWgs3H49Hi8mqnax4xnB8EHTUyqkOtK-t1BfQIoHAj3sy66M6FReAjflS1Q/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message Send Sucessfully"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 3000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
